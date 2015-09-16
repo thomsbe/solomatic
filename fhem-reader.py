7c2341b460f28a951fbebdf063fc9805a63250e8
Thomas Baer <thomas.baer@tbaer.eu>
2015-09-16 16:08:10 +0200

    Works!

--- fhem-reader.py
+++ fhem-reader.py
@@ -5,7 +5,7 @@ from solongo.rmqtools import publish
 from solongo.types import MsgClima
 
 response = urllib2.urlopen('http://officepi.solongo.office:8083/fhem?cmd=jsonlist2%20netatmo_office&XHR=1')
-data = json.load(response)
+data = json.loads(response)
 temp = data['Results'][0]['Readings']['temperature']['Value']
 hum = data['Results'][0]['Readings']['humidity']['Value']
 
